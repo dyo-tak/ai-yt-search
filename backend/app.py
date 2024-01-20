66cf8731a239c9cc3b14ee8c0e8c4e3048a8a511
Dyotak <dyotak10@gmail.com>
2024-01-20 16:10:05 +0530

    added search api

--- backend/app.py
+++ backend/app.py
@@ -1,59 +1,27 @@
-import streamlit as st
+from flask import Flask, jsonify, request
 from pinecone import Pinecone
 from sentence_transformers import SentenceTransformer
 
-@st.experimental_singleton
-def init_pinecone():
-    pinecone = Pinecone(api_key="55e1c7a7-6374-4a46-9650-b075f74e2158", environment="us-west1-gcp")
-    return pinecone.Index('youtube-search')
-    
-@st.experimental_singleton
-def init_retriever():
-    return SentenceTransformer('flax-sentence-embeddings/all_datasets_v3_mpnet-base')
 
-index = init_pinecone()
-retriever = init_retriever()
+app = Flask(__name__)
 
-def card(thubmnail, title, url, context):
-    return st.markdown(f"""
-    <div class="container-fluid">
-        <div class="row align-items-start">
-            <div class="col-md-4 col-sm-4">
-                 <div class="position-relative">
-                     <a href={url}><img src={thubmnail} class="img-fluid" style="width: 192px; height: 106px"></a>
-                 </div>
-             </div>
-             <div  class="col-md-8 col-sm-8">
-                 <a href={url}>{title}</a>
-                 <br>
-                 <span style="color: #808080;">
-                     <small>{context[:200].capitalize()+"...."}</small>
-                 </span>
-             </div>
-        </div>
-     </div>
-        """, unsafe_allow_html=True)
+pinecone = Pinecone(api_key="55e1c7a7-6374-4a46-9650-b075f74e2158", environment="us-west1-gcp")
+index = pinecone.Index('youtube-search')
 
-    
-st.write("""
-# YouTube Q&A
-Ask me a question!
-""")
+retriever = SentenceTransformer('flax-sentence-embeddings/all_datasets_v3_mpnet-base')
 
-st.markdown("""
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-""", unsafe_allow_html=True)
 
-query = st.text_input("Search!", "")
+@app.route("/search", methods=["POST"])
+def search():
+    query = request.json.get("query")
 
-if query != "":
-    xq = retriever.encode([query]).tolist()
-    xc = index.query(vector=xq, top_k=5, include_metadata=True)
-    
-    for context in xc['matches']:
-        card(
-            context['metadata']['thumbnail'],
-            context['metadata']['title'],
-            context['metadata']['url'],
-            context['metadata']['text']
-        )
+    if query:
+        xq = retriever.encode([query]).tolist()
+        xc = index.query(vector=xq, top_k=5, include_metadata=True)
+        return jsonify(xc)
+    else:
+        return jsonify({"error": "Query is required"}), 400
+
+if __name__ == "__main__":
+    app.run(debug=True)
+   
